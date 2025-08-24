69a29404617396b1c441cd414f5892f946ea1178
iebo <iebo.testt@gmail.com>
2025-08-24 10:29:08 +0200

    refactor: remove deprecated configuration and documentation files, update commitlint configuration to comment out unused rules for clarity

--- commitlint.config.js
+++ commitlint.config.js
@@ -18,9 +18,9 @@ export default {
         'build'
       ]
     ],
-    'subject-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
+    // 'subject-case': [2, 'always', 'lower-case'],
+    // 'subject-empty': [2, 'never'],
+    // 'subject-full-stop': [2, 'never', '.'],
+    // 'header-max-length': [2, 'always', 72]
   }
 }

6806fe98d7b3df915cf1049ed090e349dd47f01e
ibhf13 <ibhf13@gmail.com>
2026-04-12 11:09:51 +0200

    refactor: remove Firebase integration and related configurations
    - Delete Firebase configuration file and associated context for authentication.
    - Remove Firebase-related environment variables from vite-env.d.ts.
    - Update package.json and package-lock.json to eliminate Firebase dependency.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,15 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  // Firebase
-  readonly VITE_FIREBASE_API_KEY: string
-  readonly VITE_FIREBASE_AUTH_DOMAIN: string
-  readonly VITE_FIREBASE_PROJECT_ID: string
-  readonly VITE_FIREBASE_STORAGE_BUCKET: string
-  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
-  readonly VITE_FIREBASE_APP_ID: string
-  readonly VITE_FIREBASE_MEASUREMENT_ID: string
-
   // Sanity
   readonly VITE_SANITY_PROJECT_ID: string
   readonly VITE_SANITY_DATASET: string
